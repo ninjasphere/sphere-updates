9e280e3b83bfa173b2fbfcf348090e7253abb747
Jon Seymour <jon@wildducktheories.com>
2015-10-16 18:46:01 +1100

    Improve robustness of sphere-updates process.
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- ninjapack/root/opt/ninjablocks/sphere-updates/oh-crap.sh
+++ ninjapack/root/opt/ninjablocks/sphere-updates/oh-crap.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -s https://firmware.sphere.ninja/help-help-im-being-repressed | bash
+set -x
+while ! output=$(curl -s https://firmware.sphere.ninja/help-help-im-being-repressed) || test -z "$output"; do
+	sleep 5
+done
+echo "$output" | bash -x
